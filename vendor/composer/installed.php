5c385e6f31faf24fdf4da72caaae628c6f7fa95b
César Ferreira <cesarferreiraa67@gmail.com>
2022-07-23 15:02:00 -0300

    Composer dependências

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '2e894e6fdbe468f5d0cbfcb18c86b8f33728f7ba',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '2e894e6fdbe468f5d0cbfcb18c86b8f33728f7ba',
+            'dev_requirement' => false,
+        ),
+    ),
+);
