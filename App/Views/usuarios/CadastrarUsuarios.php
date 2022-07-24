4b94004adfe07af4290fb57a0fa605ba1f5da19f
César Ferreira <cesarferreiraa67@gmail.com>
2022-07-23 22:09:07 -0300

    Front cadastro de usuários

--- App/Views/usuarios/CadastrarUsuarios.php
+++ App/Views/usuarios/CadastrarUsuarios.php
@@ -23,49 +23,55 @@
 
 <body>
   <script src="<?php echo URL_BASE; ?>static/js/jquery.js"></script>
-  <div class="container-pagina">
-    <div id="lista_usuarios" class="w3-margin">
 
-      <h4 class="tituloPagina">Cadastro de usuários</h4>
+    <div id="lista_usuarios" class="w3-margin sessao-pagina">
 
+      <div class="topoDadosPagina">
+        <h4 class="tituloPagina">Cadastro de usuários</h4>
+        <a href="<?php echo URL_BASE; ?>usuarios" id="botaoVoltar" class="w3-button w3-margin-bottom" href="">Voltar a listagem</a>
+      </div>
+      
       <div style="display: none;">
         <label>Usuário 1</label>
       </div>
 
-      <div class="container-input w3-margin-top">
-        <label>Nome</label>
-        <input type="text" class="w3-input w3-block w3-border">
+      <div class="w3-row area-inputs">
+          <div class="container-input w3-margin-top">
+            <label>Nome</label>
+            <input type="text" class="w3-input w3-block w3-border">
+          </div>&nbsp;&nbsp;&nbsp;
+
+          <div class="container-input w3-margin-top">
+            <label>Login</label><br>
+            <input type="text" class="w3-input w3-block w3-border">
+          </div>&nbsp;&nbsp;&nbsp;
+
+          <div class="w3-margin-top">
+            <label>Senha</label><br>
+            <input type="text" class="w3-input w3-block w3-border">
+          </div>&nbsp;&nbsp;&nbsp;
       </div>
 
-      <div class="container-input w3-margin-top">
-        <label>Login</label><br>
-        <input type="text" class="w3-input w3-block w3-border">
+      <div class="w3-row area-inputs">
+          <div class="container-select w3-margin-top">
+            <label>Ativo</label><br>
+            <!-- <input type="text" class="w3-input w3-block w3-border "> -->
+            <select class="w3-input w3-block w3-border" name="" id="">
+              <option value="">Sim</option>
+              <option value="">Não</option>
+            </select>
+          </div>
+          <ul id="listaCheckBox" class="areaChecks">
+            <li id="opt_cadastrar_clientes"><input type="checkbox" checked value="cadastrar_clientes"> <label>Cadastrar clientes</label></li>
+            <li id="opt_excluir_clientes"><input type="checkbox" value="excluir_clientes"> <label>Excluir clientes</label></li>
+            <li id="opt_mais"><input type="checkbox" value="mais"> <label>E assim sucessivamente</label></li>
+          </ul>
       </div>
 
-      <div class="container-input w3-margin-top">
-        <label>Senha</label><br>
-        <input type="text" class="w3-input w3-block w3-border">
+      <div class="w3-row area-inputs w3-margin-top">
+          <button class="w3-button w3-theme w3-margin-top w3-margin-bottom">Gravar</button>
+          &nbsp;&nbsp;<a href="<?php echo URL_BASE; ?>usuarios" class="w3-button w3-margin-top w3-margin-bottom w3-right">Cancelar</a>
       </div>
-
-      <div class="container-input w3-margin-top">
-        <label>Ativo</label><br>
-        <!-- <input type="text" class="w3-input w3-block w3-border "> -->
-        <select class="w3-input w3-block w3-border" name="" id="">
-          <option value="">Sim</option>
-          <option value="">Não</option>
-        </select>
-      </div>
-
-      <ul id="listaCheckBox" class="">
-        <li id="opt_cadastrar_clientes"><input type="checkbox" checked value="cadastrar_clientes"> <label>Cadastrar clientes</label></li>
-        <li id="opt_excluir_clientes"><input type="checkbox" value="excluir_clientes"> <label>Excluir clientes</label></li>
-        <li id="opt_mais"><input type="checkbox" value="mais"> <label>E assim sucessivamente</label></li>
-      </ul>
-
-      <button class="w3-button w3-theme w3-margin-top w3-margin-bottom">Gravar</button>
-      <a href="<?php echo URL_BASE; ?>usuarios" class="w3-button w3-margin-top w3-margin-bottom w3-right">Cancelar</a>
-
-    </div>
   </div>
 </body>
 
