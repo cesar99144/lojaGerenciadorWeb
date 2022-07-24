a4bbabdf2ea89d8fea1026f02104b480e9a75c0e
César Ferreira <cesarferreiraa67@gmail.com>
2022-07-23 22:09:53 -0300

    Update Usuarios.php

--- App/Controllers/Usuarios.php
+++ App/Controllers/Usuarios.php
@@ -1,8 +1,16 @@
 <?php
 
 use App\Core\BaseControllers;
+use App\Auth;
 
 class Usuarios extends BaseControllers{
+
+    protected $ModelUsuarios;
+
+    public function __construct(){
+        
+        $this->modelUsuarios = $this->model('UsuariosDao');
+    }
 
     public function index(){
 
@@ -12,5 +20,27 @@ class Usuarios extends BaseControllers{
     public function new(){
 
         return $this->viewDash('usuarios/CadastrarUsuarios');
+    }
+
+    public function login(){
+
+        $mensagem = array();
+
+        if(isset($_POST['userLogin']) && isset($_POST['userSenha'])):
+
+			$mensagem[] = Auth::login($_POST['userLogin'], $_POST['userSenha']);
+
+			$this->view('login', $dados = ['mensagem' => $mensagem]);
+
+		endif;
+
+        // $this->view('home/index');
+    }
+
+    public function listaTodosUsuarios(){
+
+        $listaUsuarios = $this->modelUsuarios->getTodosUsers();
+
+        echo json_encode($listaUsuarios);
     }
 }
